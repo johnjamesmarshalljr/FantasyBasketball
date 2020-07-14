30ff8e24e7e7583d30e5f20883e2f55f763b9edf
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-07-14 15:33:30 -0500

    fixed relationships

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ApplicationRecord
     has_many :teams
-    has_many :users through :teams
+    has_many :users, through: :teams
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :teams
-  has_many :players through :teams
+  has_many :players, through: :teams
 end
