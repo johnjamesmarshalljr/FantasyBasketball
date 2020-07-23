9d2cac499ddeabc700c9e201c111c4df56712a50
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-07-23 07:53:34 -0500

    DRY'd routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
   resources :teams, except: :show
   resources :teams, only: :show do
-    resources :player_teams, only: [:new, :index, :show, :create]
+    resources :player_teams, only: :index
   end
-  resources :players
+  resources :players, only: :index
   resources :users, only: :show do
     resources :teams, except: :patch
   end
