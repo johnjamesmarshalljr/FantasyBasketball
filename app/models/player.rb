300907e4a16df297bd406d44655ab140626ab76f
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-07-24 07:18:07 -0500

    readability

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,20 +2,20 @@ class Player < ApplicationRecord
     has_many :teams
     has_many :users, through: :teams
 
-def self.get_guards
-  self.where("position LIKE ? ", "G")
-end
+    def self.get_guards
+      self.where("position LIKE ? ", "G")
+    end
 
-def self.get_forwards
-  self.where("position LIKE ? ", "F")
-end
+    def self.get_forwards
+      self.where("position LIKE ? ", "F")
+    end
 
-def self.get_centers
-  self.where("position LIKE ? ", "C")
-end
+    def self.get_centers
+      self.where("position LIKE ? ", "C")
+    end
 
-def fullname
-  firstName + ' '+  lastName
-end
+    def fullname
+      firstName + ' '+  lastName
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 data = HTTParty.get('http://data.nba.net/10s/prod/v1/2016/players.json')
-
 
 players = data['league']['standard'].each do |p|
 p =  Player.new(firstName: p['firstName'],
@@ -17,11 +9,3 @@ p =  Player.new(firstName: p['firstName'],
         position: p['pos'])
         p.save unless p.position.empty?
 end
-
-# 
-# [{firstName: "-- Center", position: "C"},
-#   {firstName: "-- Forward", position: "F"},
-#   {firstName: "-- Guard", position: "G"}
-# ].each do |h|
-#   Player.create(firstName: h[:firstName], lastName: "Players --")
-# end
